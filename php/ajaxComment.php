acd2b62896934e5d048174b43408cc489b70becb
xueyutianlang <xueyutianlang@163.com>
2014-12-16 15:00:55 +0800

    ajax

--- php/ajaxComment.php
+++ php/ajaxComment.php
@@ -1,5 +1,23 @@
-<?php 
-    /*ajax评论*/
+<?php
+/**
+ * discription : 评论管理列表
+ *
+ * @author : zhanghuan<xueyutianlang@163.com>
+ * @date   : 2013-11-22
+ */
+class ajaxComment extends MY_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+    }	
+	
+	
+	/* 评论ajax方式
+	 *
+	 * @param  : [int]  - $start - 偏移量
+	 * @return : [array] - $data  - 数据
+	 */
     function ajax()
     {
         $star = '';/*偏移量*/
@@ -15,5 +33,6 @@
         else
         {
             echo json_encode(array('status'=>2,'error'=>'no record or read table error'));
-        }
-	
+		}
+    }
+}
