71dbd303ea5b6dbb6ac726276d6f3346d3a25446
xueyutianlang <xueyutianlang@163.com>
2015-01-05 15:04:24 +0800

    update

--- php/mkdir.php
+++ php/mkdir.php
@@ -20,5 +20,24 @@ function createPath($dir,$mode,$recursive=false)
 {
 	if(is_dir($dir) || @mkdir($dir,$mode,$recursive)) return true;
 }
-echo createPath('/home/data/ceshi/php/a/b/c',0755);
+
+/*
+ * @多级递归创建目录
+ * @author : web 
+ * @date   : 2015-01-05
+ * @demo   : mkdirs();value : a/b/c
+ *
+ * @param  : [string] - $dir  - 创建的目录
+ * @param  : [int]    - $mode - 设置权限数值
+ * @return : [bool]   - true/false
+ */
+function mkdirs($dir,$mode=0777){
+	if(is_dir($dir)||@mkdir($dir,$mode)){
+		return true;
+	}
+	if(!mkdirs(dirname($dir),$mode)){
+		return false;
+	}
+	return @mkdir($dir,$mode);
+}
 ?>
