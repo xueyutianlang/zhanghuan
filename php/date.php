3e61f9c9766932694f2237a21f53ea7c9ede0b3a
xueyutianlang <xueyutianlang@163.com>
2015-01-07 13:58:06 +0800

    date

--- php/date.php
+++ php/date.php
@@ -27,15 +27,7 @@ function date_replace()
  * */
 function dates()
 {
-	$arr = array(
-		0=>'星期日',
-		1=>'星期一',
-        2=>'星期二',
-        3=>'星期三',
-        4=>'星期四',
-        5=>'星期五',
-        6=>'星期六',
-	);
+	$arr = array('星期日','星期一','星期二','星期三','星期四','星期五','星期六');
 	if(array_key_exists(date(w),$arr))
 	{
 		return "今天是".$arr[date(w)];
@@ -47,4 +39,3 @@ function dates()
 }
 ?>
 
-?>
