281983bc29bef57047bd7c683351b928e69db9b5
xueyutianlang <xueyutianlang@163.com>
2015-01-07 13:54:29 +0800

    date

--- php/date.php
+++ php/date.php
@@ -19,4 +19,32 @@ function date_replace()
 {
    return str_replace(array('-',' ',':'),'',date('Y-m-d H:i:s'));
 }
+
+/*
+ * @格式化时间日期字符串,显示星期
+ * @author : zhanghuan
+ * @date
+ * */
+function dates()
+{
+	$arr = array(
+		0=>'星期日',
+		1=>'星期一',
+        2=>'星期二',
+        3=>'星期三',
+        4=>'星期四',
+        5=>'星期五',
+        6=>'星期六',
+	);
+	if(array_key_exists(date(w),$arr))
+	{
+		return "今天是".$arr[date(w)];
+	}
+	else
+	{
+		return "您好:),今天为您显示火星上的日期 ^_^";
+	}
+}
+?>
+
 ?>
