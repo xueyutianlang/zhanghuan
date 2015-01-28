cda4f5f7e755362f2ac66faee2198e5646e573df
xueyutianlang <xueyutianlang@163.com>
2015-01-28 15:24:33 +0800

    array

--- php/array.php
+++ php/array.php
@@ -14,4 +14,23 @@ function statistical()
 	$array = array('1','3','1','1','3','5');
 	return array_count_values($array);
 }
+
+/*
+ * @数组指针应用
+   ★current():取得目前指针位置的内容资料。
+   ★key():读取目前指针所指向资料的索引值（键值）。
+   ★next():将数组中的内部指针移动到下一个单元。
+   ★prev():将数组的内部指针倒回一位。
+   ★end():将数组的内部指针指向最后一个元素。
+   ★reset():将目前指针无条件移至第一个索引位置。
+ *
+ */   
+   function arrPoint()
+   {
+	   $array=array(1,2,3,4,5,6);
+	   echo key($array).'|'.current($array);
+	   next($array);
+	   echo key($array).'|'.current($array);
+   }
+
 ?>
