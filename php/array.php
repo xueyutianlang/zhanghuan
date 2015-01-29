b4f1bd9c79a40a3089705bc9cf525ad36ddbd825
xueyutianlang <xueyutianlang@163.com>
2015-01-29 11:37:24 +0800

    array

--- php/array.php
+++ php/array.php
@@ -5,14 +5,15 @@
  * @date       : 2015-01-28
  *
  **/
+ 
 /*
  * @统计数组中元素出现的个数，并将统计数组进行ksort 排序
  *  
  */
 function statistical()
 {
-	$array = array('1','3','1','1','3','5');
-	return array_count_values($array);
+     $array = array('1','3','1','1','3','5');
+     return array_count_values($array);
 }
 
 /*
@@ -25,13 +26,13 @@ function statistical()
    ★reset():将目前指针无条件移至第一个索引位置。
  *
  */   
-   function arrPoint()
-   {
-	   $array=array(1,2,3,4,5,6);
-	   echo key($array).'|'.current($array);
-	   next($array);
-	   echo key($array).'|'.current($array);
-   }
+function arrPoint()
+{
+      $array=array(1,2,3,4,5,6);
+      echo key($array).'|'.current($array);
+      next($array);
+      echo key($array).'|'.current($array);
+}
 
 /*
  * @在数组中特定位置插入特定元素
@@ -39,6 +40,7 @@ function statistical()
  */
 function insert($arr=array(1,2,3,4,5),$i=3)
 {
+<<<<<<< HEAD
     $arrNew = array();
 	foreach($arr as $key=>$value)
     {
@@ -49,6 +51,22 @@ function insert($arr=array(1,2,3,4,5),$i=3)
 		$arrNew[]=$value;
 	}
 	return $arrNew
+=======
+     $arrOne = array();
+     $arrTwo = array();
+     foreach($arr as $key=>$value)
+     {
+	  if($key<$i)
+	  {
+		array_push($arrOne,$value);
+	  }
+	  else
+	  {
+		array_push($arrTwo,$value);
+	  }
+	}
+     return array_merge($arrOne,$arrTwo);
+>>>>>>> 5b83078a8ee319b448c28e75420bc18afcba63ca
 }
 
 ?>
