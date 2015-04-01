1cb8fe2b992ea9ea2cf4a5045204896b5b203c21
xueyutianlang <xueyutianlang@163.com>
2015-04-01 13:12:02 +0800

    redis-update

--- REDIS/index.php
+++ REDIS/index.php
@@ -1,9 +1,17 @@
 <?php 
-echo phpinfo();
-$hostname = "myhost";
-    $port = 10060;
-    $dbname = "tempdb";
-        $username = "dbuser";
-        $pw = "password";
-            $dbh = new PDO ("dblib:host=$hostname:$port;dbname=$dbname","$username","$pw");
+/**
+ * @descrition: redis 测试接口
+ * @author    : zhanghuan<xueyutianlang@163.com>
+ * @date      : 2015.04.01
+ *
+ **/
+$redis = new Redis();
+//链接redis
+$redis -> connect('127.0.01',6379,1);
+//$redis = new Client($server);
+//print_r($redis);die;
+//普通set/get操作
+$redis->set(1, 2);
+$retval = $redis->get(1);
+echo $retval; //显示
 ?>
